b722478cf917b48b9d25c7831f943fb87add8af8
dysett <p.denys0557@gmail.com>
2026-06-22 22:23:01 +0300

    Allow ngrok header in CORS

--- src/main/java/ua/finalproject/chat/http/ChatHttpServer.java
+++ src/main/java/ua/finalproject/chat/http/ChatHttpServer.java
@@ -1,4 +1,4 @@
-﻿package ua.finalproject.chat.http;
+package ua.finalproject.chat.http;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
